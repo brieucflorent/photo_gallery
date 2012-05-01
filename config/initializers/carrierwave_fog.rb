f8f64aced7dfc98d37fad5bb26ba231153935758
brieuc <brieucflorent@gmail.com>
2012-05-01 09:31:58 -0700

    awsbucket

--- config/initializers/carrierwave_fog.rb
+++ config/initializers/carrierwave_fog.rb
@@ -5,7 +5,7 @@ CarrierWave.configure do |config|
     :aws_secret_access_key  => 'QlSASWrFv46FEqLoV3WY/wuYW3cDU+7GFYe+GDLZ',       # required
     :region                 => 'us-east-1'  # optional, defaults to 'us-east-1'
   }
-  config.fog_directory  = 'radiant.gallery'                     # required
+  config.fog_directory  = 'radiantdawngallery'                     # required
   #config.fog_host       = 'https://assets.example.com'            # optional, defaults to nil
   config.fog_public     = false                                   # optional, defaults to true
   config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
