5b5e8aef1e6d72e33e1b57bb54b0915328ac030e
brieuc <brieucflorent@gmail.com>
2012-06-25 11:13:40 -0700

    appcache

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,6 +9,7 @@ Rottenpotatoes::Application.configure do
   # Log error messages when you accidentally call methods on nil.
   config.whiny_nils = true
 
+  config.static_cache_control = "public, max-age=2592000"
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
