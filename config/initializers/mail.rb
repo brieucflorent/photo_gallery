9ff0996aa515c2434d00bbdb1488c65254a489b1
brieuc <brieucflorent@gmail.com>
2012-09-02 14:09:17 -0700

    sendgrid config

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,9 +1,9 @@
 ActionMailer::Base.smtp_settings = {
   :address        => 'smtp.sendgrid.net',
-  :port           => '587',
+  :port           => '25',
   :authentication => :plain,
-  :user_name      => 'app3705673@heroku.com',
-  :password       => 'b5qhhfwf',
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
   :domain         => 'heroku.com'
 }
 ActionMailer::Base.delivery_method = :smtp
